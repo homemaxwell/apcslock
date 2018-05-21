80c20e030199e1eed1596baf36a4f9fa293fc0ce
homemaxwell <homemaxwell@gmail.com>
2018-05-21 11:55:12 -0700

    added isLock() and comboChange() to Lock class

---
+++ Lock.java
@@ -0,0 +1,21 @@
+public class Lock
+{
+    /**
+     * isLock() - depending on whether the combination is correct or wrong
+     * @return true/false
+     */
+    public boolean isLock()
+    {
+        boolean result = true;
+
+        return result;
+    }
+
+    /**
+     *
+     */
+    public changeCombo()
+    {
+
+    }
+}
