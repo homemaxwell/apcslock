f5e74bc0c831db0db782c484036a53196cc065a5
yamanchaudhary <yaman.chaudhary2013@gmail.com>
2018-06-07 07:13:37 -0700

    Update Driver.java

--- MAC/Driver.java
+++ MAC/Driver.java
@@ -11,14 +11,19 @@ public class Driver
 {
     public static void main(String[] args)
     {
-        File f = new File("saveData.txt");  // gets file "saveData.txt" which contains usernam, password, and security questions if the program has been run before  
+        /* gets file "saveData.txt" which contains usernam, password, and security questions 
+        if the program has been run before  */
+        File f = new File("saveData.txt");  
+        
+        // if the file was found (meaning the program has been run before thus do not ask for new username and password)
         if (f.exists())
         {
-            UserInteraction i = new UserInteraction("saveData.txt");
+            UserInteraction i = new UserInteraction("saveData.txt"); // constructs without activating firstUser
         }
+        // if the file doesn't exist, then it is the user's first time, which means that they need to set up an account 
         else
         {
-            UserInteraction e = new UserInteraction();
+            UserInteraction e = new UserInteraction(); // constructs with activating firstUser
         }
     }
 }
