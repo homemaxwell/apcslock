4d5bfd10f47892860b6d16c123ecead88e08913b
Maxwell Chang <homemaxwell@gmail.com>
2018-06-07 10:35:10 -0700

    Update WirelessPi.java

--- Pi/WirelessPi.java
+++ Pi/WirelessPi.java
@@ -20,32 +20,41 @@ public class WirelessPi {
         ServerSocket listener = new ServerSocket(9090); // ServerSocket listener will constantly listen to port 9090 inorder to receive input from MAC
         String in = null; // in is the string type variable to 
         Socket socket = null;
+        boolean newTurn = false;
         Trigger trigger = new Trigger(); // instantiates object trigger, so that we can use functions from trigger class
         try {
-            while (in == null) {
-                socket = listener.accept(); 
-                try {
-                    input = new BufferedReader(new InputStreamReader(socket.getInputStream())); // tries to receive input from inputstream
-                } 
-                finally {
-                    in = input.readLine(); // once the input is received it is stored in variable in
-                    socket.close(); // closes socket and connection
+            while (true)
+            {
+                while (in == null) {
+                    socket = listener.accept(); 
+                    try {
+                        input = new BufferedReader(new InputStreamReader(socket.getInputStream())); // tries to receive input from inputstream
+                    } 
+                    finally {
+                        in = input.readLine(); // once the input is received it is stored in variable in
+                        socket.close(); // closes socket and connection
+                        newTurn = true;
+                    }
                 }
-            }
-            if(in.equals("1")) // if input receieved from socket is string "1" then it will trigger the unlock() method from trigger class
-            {
-                System.out.println("unlock" );
-                trigger.unlock();
-            }
-            if(in.equals("0")) // if input received from socket is string "0" then it will trigger the lock() method from trigger class
-            {
-                System.out.println("lock");
-                trigger.lock();
-            }
-            if(in.equals("2")) // if input received from scoekt is string "2" then it will trigger the alarm() method from trigger class
-            {
-                System.out.println("alarm");
-                trigger.alarm();
+                if (newTurn)
+                {
+                    if(in.equals("1")) // if input receieved from socket is string "1" then it will trigger the unlock() method from trigger class
+                    {
+                        System.out.println("unlock" );
+                        trigger.unlock();
+                    }
+                    if(in.equals("0")) // if input received from socket is string "0" then it will trigger the lock() method from trigger class
+                    {
+                        System.out.println("lock");
+                        trigger.lock();
+                    }
+                    if(in.equals("2")) // if input received from scoekt is string "2" then it will trigger the alarm() method from trigger class
+                    {
+                        System.out.println("alarm");
+                        trigger.alarm();
+                    }
+                    newTurn = false;
+                }
             }
         }
         finally {
